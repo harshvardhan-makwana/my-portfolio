cf1f3675ce47d4241ff66f4e4d3865713129deee
Harshvardhan Makwana <makwanaharshvardhan338@gmail.com>
2026-06-03 20:47:45 +0530

    portfolio update

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
 export default function Home() {
-  return (
-    <div className="bg-gray-950">
+  return (<div className="bg-gray-950">
+    
       <section id="home" className="relative min-h-screen flex items-center justify-center bg-gradient-to-b from-gray-900 via-gray-900 to-black px-6 overflow-hidden">
         <div className="absolute top-1/3 left-1/2 -translate-x-1/2 w-[600px] h-[600px] bg-blue-600/20 rounded-full blur-[120px] "></div>
 
@@ -11,12 +11,12 @@ export default function Home() {
             MERN STACK DEVELOPER
           </p>
 
-          <h1 className="text-5xl md:text-7xl font-bold mb-4 leading-tight opacity-60">
-            Hi, I'm <span className="text-blue-500"> Harshvardhan</span>
+          <h1 className="text-5xl md:text-7xl font-bold mb-4 leading-tight opacity-60 text-blue-500">
+            Hi, I'm Harshvardhan
           </h1>
 
           <h2 className="text-3xl md:text-4xl font-semibold mb-6 text-gray-300">
-            I build things for <span className="text-blue-500">web</span>
+            I build things for web
           </h2>
 
           <p className="text-gray-400 text-lg md:text-xl mb-10 leading-relaxed">
